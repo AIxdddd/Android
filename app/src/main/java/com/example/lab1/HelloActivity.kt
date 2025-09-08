a8e05112781751135e8d497fcca5feadf0215f7a
Ilya <i.e.agapov.king@mail.ru>
2025-09-08 21:53:22 +0300

    Lab1

---
+++ app/src/main/java/com/example/lab1/HelloActivity.kt
@@ -0,0 +1,27 @@
+package com.example.lab1
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+import org.w3c.dom.Text
+
+class HelloActivity: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_helloact)
+        val b = intArrayOf(0,0);
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val text = findViewById<TextView>(R.id.textView)
+        button1.setOnClickListener {
+            b[0]++;
+            text.text="Нажатий на левую: " + b[0] + "\nНажатий на правую: " + b[1]
+        }
+        button2.setOnClickListener {
+            b[1]++;
+            text.text="Нажатий на левую: " + b[0] + "\nНажатий на правую: " + b[1]
+        }
+
+    }
+}
