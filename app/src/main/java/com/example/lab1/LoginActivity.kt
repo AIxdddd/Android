06bb0afdc91f15799250194dea2f1b8d27eb53b1
Ilya <i.e.agapov.king@mail.ru>
2025-10-06 18:54:32 +0300

    Lab2 доп задание

--- app/src/main/java/com/example/lab1/LoginActivity.kt
+++ app/src/main/java/com/example/lab1/LoginActivity.kt
@@ -3,6 +3,8 @@ package com.example.lab1
 import android.util.Log
 import android.app.Activity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.CheckBox
@@ -22,6 +24,33 @@ class LoginActivity:Activity() {
         val image = findViewById<ImageButton>(R.id.imageButton)
         val addButton = findViewById<Button>(R.id.button3)
 
+        // Изначально блокируем кнопку
+        addButton.isEnabled = false
+
+        // Добавляем слушатель изменений текста в поле логина
+        logField.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Проверяем наличие символа "@" в логине
+                val loginText = s?.toString() ?: ""
+                val hasAtSymbol = loginText.contains("@")
+
+                // Блокируем или разблокируем кнопку в зависимости от наличия "@"
+                addButton.isEnabled = hasAtSymbol
+
+                // Можно также добавить визуальную обратную связь
+                if (!hasAtSymbol && loginText.isNotEmpty()) {
+                    logField.error = "Логин должен содержать символ @"
+                } else {
+                    logField.error = null
+                }
+            }
+        })
 
         addButton.setOnClickListener {
             Log.i("Login","Добавляет пользователя")
